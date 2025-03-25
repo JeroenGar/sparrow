331edbcc5dbc085313e0f4915e2e85f868d8fd14
Jeroen Gardeyn <jeroen.gardeyn@hotmail.com>
2025-03-25 01:04:03 +0100

    quick experiment to have search match the paper exactly

--- src/sample/search.rs
+++ src/sample/search.rs
@@ -1,3 +1,4 @@
+use crate::config::UNIQUE_SAMPLE_THRESHOLD;
 use crate::eval::sample_eval::{SampleEval, SampleEvaluator};
 use crate::sample::best_samples::BestSamples;
 use crate::sample::coord_descent::coordinate_descent;
@@ -9,7 +10,6 @@ use jagua_rs::geometry::d_transformation::DTransformation;
 use jagua_rs::geometry::geo_traits::Shape;
 use log::debug;
 use rand::Rng;
-use crate::config::UNIQUE_SAMPLE_THRESHOLD;
 
 #[derive(Debug, Clone, Copy)]
 pub struct SampleConfig {
@@ -21,10 +21,9 @@ pub struct SampleConfig {
 pub fn search_placement(l: &Layout, item: &Item, ref_pk: Option<PItemKey>, mut evaluator: impl SampleEvaluator, sample_config: SampleConfig, rng: &mut impl Rng) -> (DTransformation, SampleEval, usize) {
     let item_min_dim = f32::min(item.shape.bbox().width(), item.shape.bbox().height());
 
-    let mut best_samples = BestSamples::new(sample_config.n_coord_descents, item_min_dim * UNIQUE_SAMPLE_THRESHOLD);
-
-    let focussed_sampler = match ref_pk {
+    let best_local_sample = match ref_pk {
         Some(ref_pk) => {
+            let mut best_samples = BestSamples::new(1, item_min_dim * UNIQUE_SAMPLE_THRESHOLD);
             //report the current placement (and eval)
             let dt = l.placed_items[ref_pk].d_transf;
             let eval = evaluator.eval(dt, Some(best_samples.worst()));
@@ -33,38 +32,53 @@ pub fn search_placement(l: &Layout, item: &Item, ref_pk: Option<PItemKey>, mut e
 
             //create a sampler around the current placement
             let pi_bbox = l.placed_items[ref_pk].shape.bbox();
-            Some(UniformBBoxSampler::new(pi_bbox, item))
+            let loc_sampler = UniformBBoxSampler::new(pi_bbox, item);
+
+            //sample around the current placement
+            for _ in 0..sample_config.n_focussed_samples {
+                let dt = loc_sampler.sample(rng);
+                let eval = evaluator.eval(dt, Some(best_samples.worst()));
+                best_samples.report(dt, eval);
+            }
+            Some(best_samples.best())
         }
         None => None,
     };
 
-    let bin_sampler = l.bin.bbox()
-        .resize_by(-item.shape.poi.radius, -item.shape.poi.radius)
-        .map(|bbox| UniformBBoxSampler::new(bbox, item));
+    let best_bin_sample = {
+        let bin_sampler = l.bin.bbox()
+            .resize_by(-item.shape.poi.radius, -item.shape.poi.radius)
+            .map(|bbox| UniformBBoxSampler::new(bbox, item));
 
-    if let Some(bin_sampler) = bin_sampler {
-        for _ in 0..sample_config.n_bin_samples {
-            let dt = bin_sampler.sample(rng).into();
-            let eval = evaluator.eval(dt, Some(best_samples.worst()));
-            best_samples.report(dt, eval);
+        let mut best_samples = BestSamples::new(1, item_min_dim * UNIQUE_SAMPLE_THRESHOLD);
+
+        if let Some(bin_sampler) = bin_sampler {
+            for _ in 0..sample_config.n_bin_samples {
+                let dt = bin_sampler.sample(rng).into();
+                let eval = evaluator.eval(dt, Some(best_samples.worst()));
+                best_samples.report(dt, eval);
+            }
+            Some(best_samples.best())
+        } else {
+            None
         }
-    }
+    };
 
-    if let Some(focussed_sampler) = focussed_sampler {
-        for _ in 0..sample_config.n_focussed_samples {
-            let dt = focussed_sampler.sample(rng);
-            let eval = evaluator.eval(dt, Some(best_samples.worst()));
-            best_samples.report(dt, eval);
+    let cd_best_sample = {
+        let mut best_samples = BestSamples::new(1, item_min_dim);
+        if let Some(best_local_sample) = best_local_sample {
+            let descended = coordinate_descent(best_local_sample, &mut evaluator, item_min_dim, rng);
+            best_samples.report(descended.0, descended.1);
+        };
+        if let Some(best_bin_sample) = best_bin_sample {
+            let descended = coordinate_descent(best_bin_sample, &mut evaluator, item_min_dim, rng);
+            best_samples.report(descended.0, descended.1);
         }
-    }
+        best_samples.best()
+    };
 
-    for start in best_samples.samples.clone() {
-        let descended = coordinate_descent(start.clone(), &mut evaluator, item_min_dim, rng);
-        best_samples.report(descended.0, descended.1);
-    }
 
-    debug!("[S] {} samples evaluated, best: {:?}, {}",evaluator.n_evals(),best_samples.best().1,best_samples.best().0);
+    debug!("[S] {} samples evaluated, best: {:?}, {}",evaluator.n_evals(),cd_best_sample.1,cd_best_sample.0);
 
-    let best_sample = best_samples.best();
-    (best_sample.0, best_sample.1, evaluator.n_evals())
+    (cd_best_sample.0, cd_best_sample.1, evaluator.n_evals())
 }
